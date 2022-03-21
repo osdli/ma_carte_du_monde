8d68d4cfc3891d5de31dd082e5694e0550aebe9d
osdli <lia.osadta@gmail.com>
2022-03-21 18:23:26 +0100

    persist&flush new user in database

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(Request $request): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -23,6 +24,9 @@ class RegisterController extends AbstractController
 
             $user = $form->getData();
 
+
+            $entityManager->persist($user);
+            $entityManager->flush();
         }
 
         return $this->render('register/index.html.twig', [
