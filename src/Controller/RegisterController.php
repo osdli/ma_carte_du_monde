5cc0a9b0af304c6d95797bb579b9eaebe36e6129
osdli <lia.osadta@gmail.com>
2022-03-21 18:15:53 +0100

    add subbmited form

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -5,16 +5,25 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegisterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+
+            $user = $form->getData();
+
+        }
 
         return $this->render('register/index.html.twig', [
             'form'=>$form->createView()
